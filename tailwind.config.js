78f2bbdb63f9ab62fd9bae3fe642aba5d5c5f576
Kasper Nilsson <kaspern@google.com>
2022-08-27 14:19:37 +0100

    Tinker more w/ theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,8 +31,7 @@ module.exports = {
           primary: '#d6d3d1', // TW stone 300
           secondary: '#d6d3d1', // TW stone 300
           accent: '#7dd3fc', // TW sky 300
-          // 'accent-content': '#075985', // TW sky 800
-          neutral: '#e7e5e4', // TW stone 200
+          neutral: '#d6d3d1', // TW stone 300
           'base-100': '#ffffff', // White
           'base-200': '#f5f5f4', // TW stone 100
           'base-300': '#d6d3d1', // TW stone 300
@@ -44,16 +43,15 @@ module.exports = {
         dark: {
           primary: '#44403c', // TW stone 700
           secondary: '#44403c', // TW stone 700
-          accent: '#0284c7', // TW sky 500 (set to opacity 0.2)
-          // 'accent-content': '#7dd3fc', // TW sky 300
-          neutral: '#292524', // TW stone 800
+          accent: '#0369a1', // TW sky 700
+          neutral: '#44403c', // TW stone 700
           'base-100': '#000000', // Black
           'base-200': '#0e0d0c', // TW stone that i made up
           'base-300': '#151312', // TW stone that i made up
-          info: '#7c3aed', // TW violet 600
-          success: '#16a34a', // TW green 600
-          warning: '#d97706', // TW amber 600
-          error: '#dc2626', // TW red 600
+          info: '#6d28d9', // TW violet 700
+          success: '#15803d', // TW green 700
+          warning: '#b45309', // TW amber 700
+          error: '#b91c1c', // TW red 700
         },
       },
     ],
