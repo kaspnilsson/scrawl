f3ac2c11e82cccab91c155576d6d749814a08d49
Kasper Nilsson <kaspern@google.com>
2022-09-08 21:02:08 +0000

    Add tasks page

---
+++ pages/t/index.tsx
@@ -0,0 +1,36 @@
+import { getUser, withPageAuth } from '@supabase/auth-helpers-nextjs'
+import useSWR from 'swr'
+import Layout from '../../components/Layout'
+import { Project } from '../../interfaces/project'
+import { fetcher } from '../../lib/apiHelpers'
+
+export const getServerSideProps = withPageAuth({
+  redirectTo: '/login',
+  async getServerSideProps(ctx) {
+    // Access the user object
+    const res = await getUser(ctx)
+    return { props: { email: res.user?.email || '' } }
+  },
+})
+
+const TasksIndex = () => {
+  const { data, error } = useSWR<Project[]>('/api/tasks', fetcher)
+
+  return (
+    <Layout
+      loading={data === undefined}
+      error={error}
+      noMaxWidth={true}
+      headerContent={
+        <div className="flex flex-wrap justify-between items-center pl-2">
+          <h3 className="flex gap-2 items-center m-0 font-heading">Tasks</h3>
+        </div>
+      }
+    >
+      <div className="m-auto prose prose-headings:!m-0 prose-headings:font-heading max-w-none">
+        WIP
+      </div>
+    </Layout>
+  )
+}
+export default TasksIndex
